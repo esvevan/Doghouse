21473f41b498bab8691000e9fb805bb5138ef4ab
esvevan <esvevan@gmail.com>
2026-02-17 13:12:34 -0800

    bootstrap fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,16 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: "127.0.0.1",
-    port: 5173
+    port: 5173,
+    proxy: {
+      "/bootstrap": {
+        target: "http://127.0.0.1:8000",
+        changeOrigin: false,
+      },
+      "/api": {
+        target: "http://127.0.0.1:8000",
+        changeOrigin: false,
+      },
+    },
   }
 });
